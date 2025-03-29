a8af73ba3d4a45672cc2c03d618aa1f62e77998d
Aend23 <ankush122324@gmail.com>
2025-03-29 23:36:21 +0530

    api end points added

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -2,6 +2,22 @@ import express from "express";
 
 const app = express();
 
+app.post("/api/v1/website", (req, res) => {
+  res.json({ message: "Hello, world!" });
+});
+
+app.get("/api/v1/website/status", (req, res) => {
+  res.json({ message: "Website is running" });
+});
+
+app.get("/api/v1/websites", (req, res) => {
+  res.json({ message: "Websites" });
+}); 
+
+app.delete("/api/v1/website/:id",   (req, res) => {
+  res.json({ message: "Website deleted" });
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
